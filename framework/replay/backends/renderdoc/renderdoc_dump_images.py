intel-lgci-fdo-gitlab-mirror/mesa.piglit

commit 4dc4ccf838e2126aa2532c98a474c7c9228e375e

framework/replay: update renderdoc against v1.20
See: https://github.com/baldurk/renderdoc/releases/tag/v1.20

Acked-by: Guilherme Gallo <guilherme.gallo@collabora.com>
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/piglit/-/merge_requests/790>

framework/replay/backends/renderdoc/renderdoc_dump_images.py
@@ -44,6 +44,11 @@ RENDERDOC_DEBUG_FILE = TMP_DIR + "/renderdoc.log"
 os.environ['RENDERDOC_DEBUG_LOG_FILE'] = RENDERDOC_DEBUG_FILE
 import renderdoc as rd  # noqa: E402
 
+try:
+    from renderdoc import ResultCode
+except ImportError:
+    from renderdoc import ReplayStatus as ResultCode
+
 
 def find_draw_with_event_id(controller, event_id):
     for d in controller.GetDrawcalls():
@@ -92,14 +97,14 @@ def load_capture(filename):
 
     status = cap.OpenFile(filename, '', None)
 
-    if status != rd.ReplayStatus.Succeeded:
+    if status != ResultCode.Succeeded:
         raise RuntimeError("Couldn't open file: " + str(status))
     if not cap.LocalReplaySupport():
         raise RuntimeError("Capture cannot be replayed")
 
     status, controller = cap.OpenCapture(rd.ReplayOptions(), None)
 
-    if status != rd.ReplayStatus.Succeeded:
+    if status != ResultCode.Succeeded:
         if os.path.exists(RENDERDOC_DEBUG_FILE):
             print(open(RENDERDOC_DEBUG_FILE, "r").read())
         raise RuntimeError("Couldn't initialise replay: " + str(status))

commit db1c07f7cee6fd939b35eb66781418b162ec7168

framework/replay: additional fixes for recent renderdoc >= v1.20
Fixes: 4dc4ccf838e2 ("framework/replay: update renderdoc against v1.20")

Reviewed-by: Sergi Blanch Torne <sergi.blanch.torne@collabora.com>
Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/piglit/-/merge_requests/809>

framework/replay/backends/renderdoc/renderdoc_dump_images.py
@@ -51,9 +51,14 @@ except ImportError:
 
 
 def find_draw_with_event_id(controller, event_id):
-    for d in controller.GetDrawcalls():
-        if d.eventId == event_id:
-            return d
+    try:
+        for root_action in controller.GetRootActions():
+            if root_action.eventId == event_id:
+                return root_action
+    except AttributeError:
+        for root_drawcall in controller.GetDrawcalls():
+            if root_drawcall.eventId == event_id:
+                return root_drawcall
 
     return None
 
@@ -78,8 +83,12 @@ def dump_image(controller, event_id, output_dir, tracefile):
     filepath.mkdir(parents=True, exist_ok=True)
     filepath = filepath / (tracefile + "-" + str(int(draw.eventId)) + ".png")
 
-    print("Saving image at event ID %d: %s to %s" % (draw.eventId,
-                                                     draw.name, filepath))
+    try:
+        filename: str = draw.GetName(controller.GetStructuredFile())
+    except AttributeError:
+        filename: str = draw.name
+
+    print(f"Saving image at event ID {draw.eventId}: {filename} to {filepath}")
 
     # Most formats can only display a single image per file, so we select the
     # first mip and first slice
@@ -122,7 +131,10 @@ def renderdoc_dump_images(filename, event_ids, output_dir):
     tracefile = Path(filename).name
 
     if not event_ids:
-        event_ids.append(controller.GetDrawcalls()[-1].eventId)
+        try:
+            event_ids.append(controller.GetRootActions()[-1].eventId)
+        except AttributeError:
+            event_ids.append(controller.GetDrawcalls()[-1].eventId)
 
     for event_id in event_ids:
         dump_image(controller, event_id, output_dir, tracefile)